20a557ce3e6eeb6dc7d2773d8e3d623e7797bad7
Kirpa Vadim <ivadik2323@gmail.com>
2017-11-11 04:49:54 +1000

    Add lua-wrapper includes, add lua lib path to cmake file

--- LandKingsServer/main.cpp
+++ LandKingsServer/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 #include "include/Engine.h"
-//#include "lib/lua/include/lua.hpp"
+#include "lib/lua/include/lua.hpp"
+#include "lib/lua-wrapper/include/luawrapper.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    Engine engine;
+    Engine::Engine engine;
     engine.run();
     return 0;
 }
