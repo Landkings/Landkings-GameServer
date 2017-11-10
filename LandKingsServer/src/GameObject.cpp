e23c252bb626c17a8ce01e308e1f6ead7c66c69c
Kirpa Vadim <ivadik2323@gmail.com>
2017-11-11 04:47:14 +1000

    Add: character move, scene update

--- LandKingsServer/src/GameObject.cpp
+++ LandKingsServer/src/GameObject.cpp
@@ -6,4 +6,8 @@ GameObject::GameObject(Position pos, HitBox hbox) : position(pos), hbox(hbox) {}
 
 Character::Character(Position pos, HitBox hbox) : GameObject(pos, hbox) {}
 
+void Character::move(Scene &scene) {
+    scene.move(this);
+}
+
 //Obstacle::Obstacle() {}
--- LandKingsServer/src/Scene.cpp
+++ LandKingsServer/src/Scene.cpp
@@ -1,8 +1,18 @@
 #include "../include/Scene.h"
-
+#include "../include/GameObject.h"
 using namespace Engine;
 
 Scene::Scene() {
     length = Constants::SCENE_LENGTH;
     width = Constants::SCENE_HEIGHT;
+}
+
+void Scene::move(GameObject* object) {
+    object->getPosition();
+}
+
+void Scene::update() {
+    for (auto object: objects) {
+
+    }
 }
