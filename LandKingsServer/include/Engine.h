588ac48d7ef2051c375602e70d1c86f74b0e3336
DBS <alekseybk2@gmail.com>
2017-12-12 00:47:18 +1000

    Delete WsServer

--- LandKingsServer/include/Engine.h
+++ LandKingsServer/include/Engine.h
@@ -8,13 +8,12 @@
 #include "Scene.h"
 #include "GameObject.h"
 #include "Position.h"
-#include "WsServer.h"
+#include "uWS.h"
 
 namespace Engine {
 
 
 class Engine {
-    friend class ::WsServer;
 public:
     Engine();
     void run();
@@ -22,15 +21,8 @@ private:
     void update();
 
     void waitForMutex(std::mutex& m, const std::chrono::microseconds& interval = std::chrono::microseconds(10));
-    void pushPendingPlayer(std::string& nickname, std::string& sourceCode);
-    void addPendingPlayers();
 
-    WsServer wsServer;
     Scene scene;
-    std::mutex sceneMutex;
-    std::vector<std::pair<std::string, std::string>> pendingPlayers;
-    std::mutex pendingPlayersMutex;
-    bool ready;
 };
 
 }
--- LandKingsServer/src/Engine.cpp
+++ LandKingsServer/src/Engine.cpp
@@ -1,10 +1,8 @@
 #include "Engine.h"
 
-#include "WsServer.h"
+static constexpr uint16_t defaultWsServerPort = 19998;
 
-static constexpr uint16_t defaultServerPort = 19999;
-
-Engine::Engine::Engine() : wsServer(*this) {}
+Engine::Engine::Engine() {}
 
 void Engine::Engine::run() {
 //    scene.addObject((PGameObject)(new Character(&scene, Position(0, 0), "p1.lua")));
@@ -15,25 +13,6 @@ void Engine::Engine::run() {
     auto lag = previous - previous;
     int cnt = 0;
 
-    bool serverRunning = true;
-    std::thread([this](bool& running)
-    {
-        running = wsServer.start(defaultServerPort);
-    },
-    std::ref(serverRunning)).detach();
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    if (serverRunning)
-        std::cout << "Server +" << std::endl;
-    else
-    {
-        std::cout << "Server -" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::exit(1);
-    }
-
-    //std::this_thread::sleep_for(std::chrono::seconds(10));
-
     while (true)
     {
         //auto current = std::chrono::system_clock::now();
@@ -41,19 +20,16 @@ void Engine::Engine::run() {
         //previous = xcurrent;
         //lag += elapsed;
 
-
-        // ***
-        waitForMutex(sceneMutex, std::chrono::microseconds(1));
-        addPendingPlayers();
-        // ***
-
         //while (lag >= 30) {
             update();
         //    lag -= 30;
         //}
 
         //scene.print();
-        sceneMutex.unlock();
+
+
+        // TODO: send objects to WsServer
+
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
@@ -68,28 +44,3 @@ void Engine::Engine::waitForMutex(std::mutex& m, const std::chrono::microseconds
         std::this_thread::sleep_for(interval);
     return;
 }
-
-// ***
-
-void Engine::Engine::pushPendingPlayer(std::string& nickname, std::string& sourceCode)
-{
-    waitForMutex(pendingPlayersMutex);
-    pendingPlayers.push_back(std::pair<std::string, std::string>(nickname, sourceCode));
-    pendingPlayersMutex.unlock();
-}
-
-void Engine::Engine::addPendingPlayers()
-{
-    if (pendingPlayers.size() == 0)
-        return;
-    waitForMutex(pendingPlayersMutex);
-    for (int i = 0; i < pendingPlayers.size(); ++i)
-    {
-        scene.addPlayer(pendingPlayers[i].first, pendingPlayers[i].second);
-                        // TODO: add players into the game
-    }
-    pendingPlayers.clear();
-    pendingPlayersMutex.unlock();
-}
-
-// ***
